71bd1a575affd1c52c524872b30118d1f6598066
ionflare <ccionflare@gmail.com>
2018-06-12 09:18:56 +0000

    init branch

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.get('/', async (req, res) => {
   var geo = await testGeoIP("58.10.224.143");  
   
   //await res.send(geo.country);
-  await res.send(req.socket.address().family);
+  await res.send(req.socket.address().address);
   
    //await replyYesNoTemplate(clientBot_2, req.body.events[0].replyToken, message, "qq");
   
